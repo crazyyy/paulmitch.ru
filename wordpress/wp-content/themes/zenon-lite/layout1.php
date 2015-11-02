bf92d5f0033780c15e6e23c0b9e83637cf7d8fe1
crazyyy <crazyyy@gmail.com>
2015-11-02 11:58:04 +0200

    ready loop

--- wordpress/wp-content/themes/zenon-lite/layout1.php
+++ wordpress/wp-content/themes/zenon-lite/layout1.php
@@ -5,52 +5,39 @@
       <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
         <div class="post_content">
           <h2 class="postitle"><a href="<?php the_permalink();?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-          <?php znn_excerpt('znn_excerptlength_index', 'znn_excerptmore'); ?>
-        </div><!-- post_content -->
+
 
         <div class="post_image">
           <!--CALL TO POST IMAGE-->
+
+          <div class="date_meta"><?php the_time('d.n.y'); ?></div>
+          <div class="block_comm">
+            <?php if (!empty($post->post_password)) { ?>
+            <?php } else { ?>
+            <div class="comments"><?php comments_popup_link( __('0</br>Comment', 'zenon'), __('1</br>Comment', 'zenon'), __('%</br>Comments', 'zenon'), '', __('Off' , 'zenon')); ?></div>
+            <?php } ?>
+          </div><!-- block_comm -->
+
           <?php if ( has_post_thumbnail() ) : ?>
-          <div class="imgwrap">
-            <div class="date_meta"><?php the_time('d'); ?><?php the_time('S'); ?> <?php the_time('M'); ?></div>
-            <div class="block_comm">
-              <?php if (!empty($post->post_password)) { ?>
-              <?php } else { ?>
-              <div class="comments"><?php comments_popup_link( __('0</br>Comment', 'zenon'), __('1</br>Comment', 'zenon'), __('%</br>Comments', 'zenon'), '', __('Off' , 'zenon')); ?></div>
-              <?php } ?>
-            </div><!-- block_comm -->
+
             <a href="<?php the_permalink();?>"><?php the_post_thumbnail('medium'); ?></a>
-          </div><!-- imgwrap -->
 
           <?php elseif($photo = znn_get_images('numberposts=1', true)): ?>
 
-          <div class="imgwrap">
-            <div class="date_meta"><?php the_time('d'); ?><?php the_time('S'); ?> <?php the_time('M'); ?></div>
-            <div class="block_comm">
-              <?php if (!empty($post->post_password)) { ?>
-              <?php } else { ?>
-              <div class="comments"><?php comments_popup_link( __('0</br>Comment', 'zenon'), __('1</br>Comment', 'zenon'), __('%</br>Comments', 'zenon'), '', __('Off' , 'zenon')); ?></div>
-              <?php } ?>
-            </div><!-- block_comm -->
             <a href="<?php the_permalink();?>"><?php echo wp_get_attachment_image($photo[0]->ID ,'medium'); ?></a>
-          </div><!-- imgwrap -->
 
           <?php else : ?>
 
-          <div class="imgwrap">
-            <div class="date_meta"><?php the_time('d'); ?><?php the_time('S'); ?> <?php the_time('M'); ?></div>
-            <div class="block_comm">
-              <?php if (!empty($post->post_password)) { ?>
-              <?php } else { ?>
-              <div class="comments"><?php comments_popup_link( __('0</br>Comment', 'zenon'), __('1</br>Comment', 'zenon'), __('%</br>Comments', 'zenon'), '', __('Off' , 'zenon')); ?></div>
-              <?php } ?>
-            </div><!-- block_comm -->
-
             <a href="<?php the_permalink();?>"><img src="<?php echo get_template_directory_uri(); ?>/images/blank_img.png" alt="<?php the_title_attribute(); ?>" class="znn_thumbnail"/></a>
-          </div><!-- imgwrap -->
 
           <?php endif; ?>
         </div><!-- post_image -->
+
+        <div class="znn_excerptmore">
+          <?php znn_excerpt('znn_excerptlength_index', 'znn_excerptmore'); ?>
+        </div><!-- /.znn_excerptmore -->
+
+        </div><!-- post_content -->
 
       </div><!-- post_class -->
     <?php endwhile ?>
